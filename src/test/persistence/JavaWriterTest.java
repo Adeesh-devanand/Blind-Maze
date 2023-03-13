f208a764f4213902409bf8adfe0144e4c47f9ea2
Adeesh Devanand <adeeshdevanand@gmail.com>
2023-03-12 22:11:49 -0700

    added Writer tests

--- src/test/persistence/JavaWriterTest.java
+++ src/test/persistence/JavaWriterTest.java
@@ -1,49 +1,64 @@
-//package persistence;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import java.io.IOException;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.fail;
-//
-//public class JavaWriterTest {
-//    @Test
-//    void testReaderNonExistentFile() {
-//        JsonReader reader = new JsonReader("./data/noSuchFile.json");
-//        try {
-//            WorkRoom wr = reader.read();
-//            fail("IOException expected");
-//        } catch (IOException e) {
-//            // pass
-//        }
-//    }
-//
-//    @Test
-//    void testReaderEmptyWorkRoom() {
-//        JsonReader reader = new JsonReader("./data/testReaderEmptyWorkRoom.json");
-//        try {
-//            WorkRoom wr = reader.read();
-//            assertEquals("My work room", wr.getName());
-//            assertEquals(0, wr.numThingies());
-//        } catch (IOException e) {
-//            fail("Couldn't read from file");
-//        }
-//    }
-//
-//    @Test
-//    void testReaderGeneralWorkRoom() {
-//        JsonReader reader = new JsonReader("./data/testReaderGeneralWorkRoom.json");
-//        try {
-//            WorkRoom wr = reader.read();
-//            assertEquals("My work room", wr.getName());
-//            List<Thingy> thingies = wr.getThingies();
-//            assertEquals(2, thingies.size());
-//            checkThingy("needle", Category.STITCHING, thingies.get(0));
-//            checkThingy("saw", Category.WOODWORK, thingies.get(1));
-//        } catch (IOException e) {
-//            fail("Couldn't read from file");
-//        }
-//    }
-//}
+package persistence;
+
+import model.exceptions.MazeAlreadyExistsException;
+import org.junit.jupiter.api.Test;
+import ui.Game;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsonWriterTest {
+    @Test
+    void testWriterInvalidFile() {
+        try {
+            Game game = new Game();
+            JsonWriter writer = new JsonWriter("./data/my\0illegal:fileName.json");
+            writer.open();
+            fail("IOException was expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void testWriterEmptyGame() {
+        try {
+            Game game = new Game();
+            JsonWriter writer = new JsonWriter("./data/testWriterEmptyWorkroom.json");
+            writer.open();
+            writer.writeGame(game);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterEmptyWorkroom.json");
+            game = reader.loadGame();
+            assertTrue(game.isEmpty());
+            assertFalse(game.isRunning());
+            assertEquals("EditMode", game.getMode());
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+
+    @Test
+    void testWriterNonEmptyGame() {
+        try {
+            Game game = new Game();
+            game.createMaze("FirstMaze", 5);
+            JsonWriter writer = new JsonWriter("./data/testWriterGeneralWorkroom.json");
+            writer.open();
+            writer.writeGame(game);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterGeneralWorkroom.json");
+            game = reader.loadGame();
+            assertFalse(game.isEmpty());
+            assertFalse(game.isRunning());
+            assertEquals("EditMode", game.getMode());
+
+        } catch (IOException | MazeAlreadyExistsException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+}
